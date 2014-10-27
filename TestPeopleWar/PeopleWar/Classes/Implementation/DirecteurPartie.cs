79c680f6535806aa019f4d595d5fcfbc8e2dff53
Hassan EL Omari Alaoui <hassan.el-omarialaoui@insa-rennes.fr>
2014-10-27 17:17:11 +0100

    Diagramme de séquence : Création d'une partie

---
+++ TestPeopleWar/PeopleWar/Classes/Implementation/DirecteurPartie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleWar
+{
+    public class DirecteurPartie
+    {
+        public Partie creerPartie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
