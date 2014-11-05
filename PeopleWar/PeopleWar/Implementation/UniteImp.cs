b516402ce53f28e311c2a3fde64c3ccf9a9def61
TeoTop <theo.c24@hotmail.fr>
2014-11-05 15:24:06 +0100

    People war organisation dossier

---
+++ PeopleWar/PeopleWar/Implementation/UniteImp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleWar
+{
+    public class UniteImp : Unite
+    {
+        public CombatImp CombatImp
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
