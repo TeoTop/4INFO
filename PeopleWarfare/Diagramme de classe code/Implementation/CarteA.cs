392cdb2e2204b0daf4559c00b1e5cfef22913cbd
TeoTop <theo.c24@hotmail.fr>
2014-11-06 00:00:03 +0100

    Eradication des problèmes de conflits + restructuration du projet

---
+++ PeopleWarfare/Diagramme de classe code/Implementation/CarteA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleWar
+{
+    public abstract class CarteA : Carte
+    {
+        public CaseA CaseA
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CaseA CaseA1
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ PeopleWarfare/Diagramme de classe code/Implementation/UniteImp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeopleWar
+{
+    public class UniteImp : Unite
+    {
+        public CombatImp CombatImp
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public CombatImp CombatImp1
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
