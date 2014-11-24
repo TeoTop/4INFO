a88525fb8889fc4b838c3a6c2a1d993d30adf040
Hassan EL OMARI ALAOUI <elomarialaoui.hassan@gmail.com>
2014-11-24 21:10:04 +0100

    tp8 prolog q2.4

--- Prolog/tp8/tal.pl
+++ Prolog/tp8/tal.pl
@@ -58,26 +58,26 @@ art(un,mas,sin).
 art(une,fem,sin).
 art(des,_,plu).
 
-nc(chien,mas,sin).
-nc(chiens,mas,plu).
-nc(enfants,mas,plu).
-nc(rue,fem,sin).
-nc(pull,mas,sin).
-nc(steack,mas,sin).
-nc(femme,fem,sin).
+nc(chien,mas,sin,animal).
+nc(chiens,mas,plu,animal).
+nc(enfants,mas,plu,humain).
+nc(rue,fem,sin,lieu).
+nc(pull,mas,sin,objet).
+nc(steack,mas,sin,objet).
+nc(femme,fem,sin,humain).
 
-np(paul,mas,sin).
+np(paul,mas,sin,humain).
 
-adj(noir,mas,sin).
+adj(noir,mas,sin,_).
 
-pre(dans).   
+pre(dans,lieu).   
 
-vb(aboie,sin).
-vb(aboient,plu).
-vb(jouent,plu).
-vb(marche,sin).
-vb(porte,sin).
-vb(mange,sin).
+vb(aboie,sin,animal,vide).
+vb(aboient,plu,animal,vide).
+vb(jouent,plu,_,_).
+vb(marche,sin,_,lieu).
+vb(porte,sin,_,objet).
+vb(mange,sin,_,objet).
 
 pn(qui).
 
@@ -98,11 +98,11 @@ verbe_arbre([X|L],L,vb(X)):-vb(X).
 pronom_arbre([X|L],L,pn(X)):-pn(X).
 */
 article_arbre([X|L],L,art(X),G,N):-art(X,G,N).
-nom_commun_arbre([X|L],L,nc(X),G,N):-nc(X,G,N).
-nom_propre_arbre([X|L],L,np(X),G,N):-np(X,G,N).
-adjectif_arbre([X|L],L,adj(X),G,N):-adj(X,G,N).
-prep_arbre([X|L],L,prep(X)):-pre(X).
-verbe_arbre([X|L],L,vb(X),N):-vb(X,N).
+nom_commun_arbre([X|L],L,nc(X),G,N,C):-nc(X,G,N,C).
+nom_propre_arbre([X|L],L,np(X),G,N,C):-np(X,G,N,C).
+adjectif_arbre([X|L],L,adj(X),G,N,C):-adj(X,G,N,C).
+prep_arbre([X|L],L,prep(X),C):-pre(X,C).
+verbe_arbre([X|L],L,vb(X),N,C1,C2):-vb(X,N,C1,C2).
 pronom_arbre([X|L],L,pn(X)):-pn(X).
 
 /*
@@ -193,12 +193,12 @@ analyse_arbre([la,femme,qui,porte,un,pull,noir,mange,un,chien],A).
 */
 
 /* Question 2.3 */
-analyse_arbre(L,A):-phrase_simple_arbre(L,[],A).
-phrase_simple_arbre(L,R,phr(A1,A2)):-gp_nominal_arbre(L,L1,A1,G,N),gp_verbal_arbre(L1,R,A2,G,N).
+/*analyse_arbre(L,A):-phrase_simple_arbre(L,[],A).
+phrase_simple_arbre(L,R,phr(A1,A2)):-gp_nominal_arbre(L,L1,A1,G,N),gp_verbal_arbre(L1,R,A2,N).
 
 gp_nominal_arbre(L,R,gn(A1,A2),G,N):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,R,A2,G,N).
 
-gp_nominal_arbre(L,R,gn(A1,A2,A3),G,N):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2),relatif_arbre(L2,R,A3,N).
+gp_nominal_arbre(L,R,gn(A1,A2,A3),G,N):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2,G,N),relatif_arbre(L2,R,A3,N).
 
 gp_nominal_arbre(L,R,gn(A1,A2,A3),G,N):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2,G,N),adjectif_arbre(L2,R,A3,G,N).
 
@@ -214,7 +214,7 @@ gp_verbal_arbre(L,R,gv(A),N):-verbe_arbre(L,R,A,N).
 gp_verbal_arbre(L,R,gv(A1,A2),N):-verbe_arbre(L,L1,A1,N),gp_prepositionnel_arbre(L1,R,A2).
 
 gp_verbal_arbre(L,R,gv(A1,A2),N):-verbe_arbre(L,L1,A1,N),gp_nominal_arbre(L1,R,A2,_,_).
-gp_prepositionnel_arbre(L,R,gp(A1,A2)):-prep_arbre(L,L1,A1),gp_nominal_arbre(L1,R,A2,_,_).
+gp_prepositionnel_arbre(L,R,gp(A1,A2)):-prep_arbre(L,L1,A1),gp_nominal_arbre(L1,R,A2,_,_).*/
 
 /* Test
 analyse_arbre([paul,marche,dans,le,rue],A).
@@ -231,4 +231,45 @@ analyse_arbre([la,femme,qui,porte,un,pull,noir,mange,un,steack],A).
 analyse_arbre([les,chiens,aboient],A).
 analyse_arbre([la,femme,qui,porte,un,pull,noir,mange,un,chien],A).              
 
+*/
+
+
+/* Question 2.4 */
+
+analyse_arbre(L,A):-phrase_simple_arbre(L,[],A).
+phrase_simple_arbre(L,R,phr(A1,A2)):-gp_nominal_arbre(L,L1,A1,G,N,C),gp_verbal_arbre(L1,R,A2,N,C,_).
+
+gp_nominal_arbre(L,R,gn(A1,A2),G,N,C):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,R,A2,G,N,C).
+
+gp_nominal_arbre(L,R,gn(A1,A2,A3),G,N,C):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2,G,N,C),relatif_arbre(L2,R,A3,N,C).
+
+gp_nominal_arbre(L,R,gn(A1,A2,A3),G,N,C):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2,G,N,C),adjectif_arbre(L2,R,A3,G,N,C).
+
+gp_nominal_arbre(L,R,gn(A1,A2,A3,A4),G,N,C):-article_arbre(L,L1,A1,G,N),nom_commun_arbre(L1,L2,A2,G,N,C),adjectif_arbre(L2,L3,A3,G,N,C),relatif_arbre(L3,R,A4,N,C).
+
+gp_nominal_arbre(L,R,gn(A),G,N,C):-nom_propre_arbre(L,R,A,G,N,C).
+
+gp_nominal_arbre(L,R,gn(A1,A2),G,N,C):-nom_propre_arbre(L,L1,A1,G,N,C),relatif_arbre(L1,R,A2,N,C).
+
+relatif_arbre(L,R,rel(A1,A2),N,C):-pronom_arbre(L,L1,A1),gp_verbal_arbre(L1,R,A2,N,C,_).
+gp_verbal_arbre(L,R,gv(A),N,C1,C2):-verbe_arbre(L,R,A,N,C1,C2).
+
+gp_verbal_arbre(L,R,gv(A1,A2),N,C1,C2):-verbe_arbre(L,L1,A1,N,C1,C2),gp_prepositionnel_arbre(L1,R,A2).
+
+gp_verbal_arbre(L,R,gv(A1,A2),N,C1,C2):-verbe_arbre(L,L1,A1,N,C1,C2),gp_nominal_arbre(L1,R,A2,_,_,C2).
+gp_prepositionnel_arbre(L,R,gp(A1,A2)):-prep_arbre(L,L1,A1,C),gp_nominal_arbre(L1,R,A2,_,_,C).
+
+/* Test
+analyse_arbre([paul,mange,un,steack],A).
+	A = phr(gn(np(paul)), gv(vb(mange), gn(art(un), nc(steack))))
+	Yes
+
+analyse_arbre([paul,mange,la,rue],A).
+	No
+
+analyse_arbre([paul,aboie],A).
+	No
+
+ analyse_arbre([les,enfants,jouent,dans,le,chien],A).
+	No
 */
